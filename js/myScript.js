8ceafa3931c59259f293bc5c8c3d8cf67f5b39e5
jess169 <jess.n169@hotmail.com>
2016-12-06 12:17:39 -0500

    fixed text animation

--- js/myScript.js
+++ js/myScript.js
@@ -11,7 +11,14 @@ function myFunction() {
     if ((document.body.scrollTop > 760 || document.documentElement.scrollTop > 760) && interactiveShown == false) {
        $(".bg").interactive_bg();
 	   interactiveShown = true;
+	   document.getElementById("spaceText").className = "animated delay swing";
+	   setTimeout(myFunction2, 2500);
 	}
+}
+
+
+function myFunction2() {    
+	   document.getElementById("spaceText").style.visibility = "visible";
 }
 
 
